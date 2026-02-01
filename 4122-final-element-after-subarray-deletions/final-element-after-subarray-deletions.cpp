dcd0f357e0c69206ce79325a1a50925e8c385528
Aryan Bhaskar <aryanbhaskar444@gmail.com>
2026-02-01 19:24:24 +0530

    Time: 0 ms (100.00%) | Memory: 179.9 MB (59.31%) - LeetSync

--- 4122-final-element-after-subarray-deletions/final-element-after-subarray-deletions.cpp
+++ 4122-final-element-after-subarray-deletions/final-element-after-subarray-deletions.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int finalElement(vector<int>& nums) {
-        return max(nums.front(), nums.back());
+        int m = nums.size();
+
+        if(nums[0] > nums[m-1]){
+            return nums[0];
+        }
+
+        if(m < 3){
+            return nums[m-1];
+        }
+
+        return max(nums[0],nums[m-1]);
     }
 };
