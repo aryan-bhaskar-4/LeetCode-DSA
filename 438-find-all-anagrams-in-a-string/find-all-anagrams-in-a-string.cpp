b69753cc7cf6bcc1f00502a7766f9f821c44cd15
Aryan Bhaskar <aryanbhaskar444@gmail.com>
2026-02-11 13:56:37 +0530

    Time: 1 ms (84.65%) | Memory: 11.4 MB (83.91%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,28 +1,52 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int> v;
-        if(p.size()>s.size()) return {};
-        unordered_map<char,int>m1,m2;
-
-        int i=0;
-        int j=p.size()-1;
-        for(int pp=0;pp<p.size();pp++)
-        {
-            m1[s[pp]]++;
-            m2[p[pp]]++;
+        vector<int> freqP(26, 0);
+        vector<int> freqW(26, 0); 
+        vector<int> ans;
+        
+        int k = p.size();
+        int n = s.size();
+        
+        if(k > n) return ans;
+        
+        for(char c : p){
+            freqP[c - 'a']++;
         }
-        if(m1==m2) v.push_back(i);
 
-        while(j<s.size()-1)
-        {
-            j++; 
-            m1[s[j]]++;
-            m1[s[i]]--;
-            if(m1[s[i]]==0) m1.erase(s[i]);
-            i++;
-            if(m1==m2) v.push_back(i);
+        for(int i = 0; i < k; i++){
+            freqW[s[i] - 'a']++;
         }
-        return v;
+        
+        int matches = 0;
+        for(int i = 0; i < 26; i++){
+            if(freqP[i] == freqW[i]) matches++;
+        }
+        
+        if(matches == 26){
+            ans.push_back(0);
+        }
+        
+        for(int i = k; i < n; i++){
+            int right = s[i] - 'a';      
+            int left = s[i - k] - 'a';
+            
+            
+            if(freqP[right] == freqW[right]) matches--;
+            freqW[right]++;
+
+            if(freqP[right] == freqW[right]) matches++;
+            
+            if(freqP[left] == freqW[left]) matches--;
+            freqW[left]--;
+    
+            if(freqP[left] == freqW[left]) matches++;
+            
+            if(matches == 26){
+                ans.push_back(i - k + 1);
+            }
+        }
+        
+        return ans;
     }
 };
