2e74016847dad430ef3c959f4c3ad004ffd345b5
Aryan Bhaskar <aryanbhaskar444@gmail.com>
2026-03-22 09:39:15 +0530

    Time: 3 ms (6.38%) | Memory: 31.1 MB (9.15%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -1,7 +1,78 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        return true;
-            
+        int n = nums1.size();
+        vector<int> nums2(n,0);
+        bool isAllEven = true;
+        bool isAllOdd = true;
+
+        for(int i=0;i<n;i++){
+            if(nums1[i] % 2 != 0){
+                isAllEven = false;
+            } else {
+                isAllOdd = false;
+            }
+        }
+
+        if(isAllEven){
+            return true;
+        }
+
+        if(isAllOdd){
+            return true;
+        }
+
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(nums1[i] % 2 == 0){
+                    nums2[i] = nums1[i];
+                } else if(nums1[i] % 2 != 0 && i != j){
+                    nums2[i] = nums1[i] - nums1[j];
+                    if(nums2[i] % 2 == 0){
+                        break;
+                    }
+                }
+            }
+        }
+
+        isAllEven = true;
+
+        for(int i=0;i<n;i++){
+            if(nums2[i] % 2 != 0){
+                isAllEven = false;
+            }
+        }
+
+        if(isAllEven){
+            return true;
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(nums1[i] % 2 != 0){
+                    nums2[i] = nums1[i];
+                } else if(nums1[i] % 2 == 0 && i != j){
+                    nums2[i] = nums1[i] - nums1[j];
+                    if(nums2[i] % 2 != 0){
+                        break;
+                    }
+                }
+            }
+        }
+
+        isAllOdd = true;
+
+        for(int i=0;i<n;i++){
+            if(nums2[i] % 2 == 0){
+                isAllOdd = false;
+            }
+        }
+
+        if(isAllOdd){
+            return true;
+        }
+
+        return false;  
     }
 };
